daca59f2de5ba9e7a04afdb333a6d0b0efd45eb6
Christopher Dempewolf <chris@chrisdempewolf.com>
2025-07-14 13:43:44 -0600

    Update error msg

--- src/Console/Commands/Concerns/RequiresScabbardConfig.php
+++ src/Console/Commands/Concerns/RequiresScabbardConfig.php
@@ -7,7 +7,9 @@ trait RequiresScabbardConfig
   protected function ensureScabbardConfigExists(): bool
   {
     if (! file_exists(config_path('scabbard.php'))) {
-      $this->error('Scabbard config not found. Run: php artisan vendor:publish --tag=scabbard-config');
+      $this->error('❌ Scabbard config not found.');
+      $this->line('Run the following command to publish the default config:');
+      $this->line('  php artisan vendor:publish --tag=scabbard-config');
       return false;
     }
 
