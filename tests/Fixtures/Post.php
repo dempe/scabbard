c915f51719cdec33ad2812a80539b66979a1a99f
Christopher Dempewolf <chris@chrisdempewolf.com>
2025-07-18 12:45:15 -0600

    cs:fix

--- tests/Fixtures/Post.php
+++ tests/Fixtures/Post.php
@@ -3,17 +3,18 @@
 namespace Scabbard\Tests\Fixtures;
 
 use Illuminate\Support\Collection;
+
 /**
  * Dummy model used for testing dynamic route value resolution.
  */
 
 class Post
 {
-    /**
-     * @return Collection<int, string>
-     */
-    public static function pluck(string $attribute): Collection
-    {
-        return collect(['alpha', 'beta']);
-    }
+  /**
+   * @return Collection<int, string>
+   */
+  public static function pluck(string $attribute): Collection
+  {
+    return collect(['alpha', 'beta']);
+  }
 }
